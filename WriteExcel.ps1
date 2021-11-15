129683cff8681dfe0d2b65cf2229bb45916d2b1c
robinnavinraj <64748179+robinnavinraj@users.noreply.github.com>
2021-11-15 17:28:03 +0530

    Add files via upload

---
+++ WriteExcel.ps1
@@ -0,0 +1,9 @@
+﻿$ExcelObject = new-Object -comobject Excel.Application 
+$strPath1="C:\Users\Admin\Documents\UiPath\Devlopment\Input files\Raw data.xlsx"
+$ActiveWorkbook = $ExcelObject.WorkBooks.Open($strPath1)  
+$ActiveWorksheet = $ActiveWorkbook.Worksheets.Item(1)
+$ActiveWorksheet.Cells.Item(1,15) = "robinnavinraj" 
+$ActiveWorksheet.Cells.Item(1,19) = "robinnavinraj" 
+$ActiveWorksheet.Cells.Item(1,20) = "robinnavinraj" 
+$ActiveWorkbook.Save()
+$ActiveWorkbook.close($true)
